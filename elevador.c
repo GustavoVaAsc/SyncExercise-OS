28ac16ad4a225c5f6d297654025963f6813ce701
GustavoVaAsc <quickgohan98@gmail.com>
2025-03-25 15:06:05 -0600

    Agrego el hilo que modela el elevador

--- elevador.c
+++ elevador.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <unistd.h>
+
+#ifdef _WIN32
+    #include <Windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+const int PERSONAS = 8;
+const int ELEVADOR = 1;
+const int PISOS = 5;
+
+// Variables compartidas
+int piso_actual = 0;
+int pasajeros = 0;
+int solicitudes[PISOS] = {0};
+omp_lock_t lock_elevador;
+
+void logicaElevador(){
+
+}
+
+void logicaPasajero(){
+    
+}
 
 int main(){
+
     return 0;
 }
